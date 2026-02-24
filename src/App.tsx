2a7c3c58a91777de25c5e5fb538ee58d15a482d5
MaulanaNur12 <143168440+MaulanaNur12@users.noreply.github.com>
2026-02-24 08:16:58 +0700

    Initial portfolio setup with React + TypeScript + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import MainLayout from "./layouts/MainLayout";
+import Hero from "./sections/Hero";
+
+export default function App() {
+    return (
+        <MainLayout>
+            <Hero />
+        </MainLayout>
+    );
+}
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+    return (
+        <section className="min-h-screen flex items-center justify-center text-center px-6">
+            <div>
+                <h1 className="text-5xl md:text-6xl font-bold mb-6">
+                    Hi, I'm Lananuranf
+                </h1>
+                <p className="text-gray-400 text-lg max-w-xl mx-auto">
+                    Web Developer • Data Scienc • DevSecOps Explorer
+                </p>
+            </div>
+        </section>
+    );
+}
